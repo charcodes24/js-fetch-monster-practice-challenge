899f6f82d838eee121b2c5a9f170c313996f721e
Charlene Johnson <charcodes24@gmail.com>
2021-06-02 11:41:46 -0700

    changes to JS and added form to HTML

--- js/index.js
+++ js/index.js
@@ -12,9 +12,29 @@ const monstersAPI = 'http://localhost:3000/monsters';
 function getMonsters() {
     return fetch(monstersAPI)
     .then(res => res.json())
-    .then(data => {
-        data.forEach(element => {
-            console.log(element.name)
-        })
-    })
+    .then(data => displayData(data));
 }
+
+//function to display data
+function displayData(data) {
+    for (let i = 0; i <= 49; i++) {
+        //create div for each monster element w id = id#
+        const newDiv = document.createElement('div');
+        //create H2 for name & set innerText
+        const name = document.createElement('h2');
+        name.innerText = data[i].name;
+        console.log(name)
+        //create H4 for age & set innerText
+        const age = document.createElement('h4');
+        age.innerText = data[i].age;
+        //create p for description
+        const desc = document.createElement('p');
+        desc.innerText = data[i].description;
+        //append to div
+        newDiv.append(name, age, desc);
+        monsterContainer.append(newDiv);
+        
+        }
+    }
+   
+
